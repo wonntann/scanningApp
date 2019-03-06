7821f15a21514378fd45228ec155c5a2499f31f4
Tanya Pulley <atpulley@gmail.com>
2019-03-06 12:32:47 -0800

    Load image and resize

--- scan.py
+++ scan.py
@@ -11,4 +11,23 @@ ap = argparse>ArgumentParser()
 ap.add_argument("-i", "--image", required = True, 
     help = "Path to the image to be scanned")
 args = vars(ap.parse_args())
-
+	
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+ 
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+ 
+# show the original image and the edge detected image
+print("STEP 1: Edge Detection")
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
