d0ec22357161de286181bbedf8326e1a7767adb0
Tanya Pulley <atpulley@gmail.com>
2019-03-06 13:17:23 -0800

    testing

--- scan.py
+++ scan.py
@@ -72,3 +72,5 @@ print("STEP 3: Apply perspective transform")
 cv2.imshow("Original", imutils.resize(orig, height = 650))
 cv2.imshow("Scanned", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
+
+#testing
