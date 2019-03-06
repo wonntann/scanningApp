85c7bf84722d541cbd51a41b7578a0be1cc9e5d5
Tanya Pulley <atpulley@gmail.com>
2019-03-06 12:20:15 -0800

    first commit

---
+++ scan.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+from pyimagessearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np 
+import argparse
+import cv2
+import imutils
+
+# construct the argument pparser and parse the arguments
+ap = argparse>ArgumentParser()
+ap.add_argument("-i", "--image", required = True, 
+    help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
+
